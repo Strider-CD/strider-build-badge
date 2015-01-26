cd554b30e5237aef711e6d08770f3bba99937b9b
Ilya Radchenko <ilya@burstcreations.com>
2015-01-26 11:38:32 -0500

    Use path.join

--- webapp.js
+++ webapp.js
@@ -1,17 +1,22 @@
-var getBuildStatus = require('./lib/build-status')
+'use strict';
+
+var path = require('path');
+var getBuildStatus = require('./lib/build-status');
 
 module.exports = function (context, done) {
   context.app.get('/:org/:repo/badge', function (req, res) {
-    var name = req.params.org + '/' + req.params.repo
-    var branch = req.query.branch
+    var name = req.params.org + '/' + req.params.repo;
+    var branch = req.query.branch;
 
     getBuildStatus(name, branch, context, function (error, imageName) {
       if (error) {
-        console.error('[badge] error occured when getting badge: ' + error.message)
+        console.error('[badge] error occured when getting badge: ' + error.message);
       }
-      res.setHeader('Cache-Control', 'no-cache')
-      res.sendfile(__dirname + '/images/' + imageName)
-    })
-  })
-  done()
-}
+      
+      res.setHeader('Cache-Control', 'no-cache');
+      res.sendfile(path.join(__dirname, 'images', imageName));
+    });
+  });
+  
+  done();
+};
