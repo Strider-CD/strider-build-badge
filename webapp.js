ca2ade3c3854fe8ac00f735e6fc8dd92cfb35c5b
James Lambie <jameslambie@gmail.com>
2016-06-30 14:45:48 +0800

    feat: add switch for sending image for svg badge

--- webapp.js
+++ webapp.js
@@ -5,18 +5,38 @@ var getBuildStatus = require('./lib/build-status');
 
 module.exports = function (context, done) {
   context.app.get('/:org/:repo/badge', function (req, res) {
-    var name = req.params.org + '/' + req.params.repo;
-    var branch = req.query.branch;
+    var name = req.params.org + '/' + req.params.repo
+    var branch = req.query.branch
+    var style = req.query.style || 'default'
 
-    getBuildStatus(name, branch, context, function (error, imageName) {
+    getBuildStatus(name, branch, context, function (error, status) {
       if (error) {
-        console.error('[badge] error occured when getting badge: ' + error.message);
+        console.error('[badge] error occured when getting badge: ' + error.message)
       }
-      
-      res.setHeader('Cache-Control', 'no-cache');
-      res.sendfile(path.join(__dirname, 'images', imageName));
-    });
-  });
-  
+
+      if (style === 'default') {
+        res.sendfile(path.join(__dirname, 'images', 'build_' + status.label + '.png'));
+      } else {
+
+        var badge = {
+          subject: 'build',
+          style: 'flat-square'
+        }
+
+        var badgeUrl = badge.subject + '-' + status.label + '-' + status.color + '.svg?style=' + badge.style;
+
+        var externalReq = require('https').request({
+          protocol: 'https:',
+          hostname: 'img.shields.io',
+          path: '/badge/' + badgeUrl
+        }, function(externalRes) {
+          res.setHeader('Cache-Control', 'no-cache');
+          res.setHeader('content-type', 'image/svg+xml;charset=utf-8');
+          externalRes.pipe(res);
+        })
+        externalReq.end();
+      }
+    })
+  })
   done();
 };
