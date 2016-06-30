abc3ad44edbc7445ac19e29b8236875853cfa362
James Lambie <jameslambie@gmail.com>
2016-06-30 14:46:05 +0800

    feat: add tests and update readme

--- test/build-status.test.js
+++ test/build-status.test.js
@@ -1,4 +1,5 @@
 var expect = require('expect.js')
+  , sinon = require('sinon')
   , getBuildStatus = require('../lib/build-status')
 
 describe('Build Status', function () {
@@ -19,42 +20,73 @@ describe('Build Status', function () {
     return { models: { Job: jobModel } }
   }
 
-  it('should return success image with successful build', function (done) {
+  it('should return success label with successful build', function (done) {
     var job = { test_exitcode: 0 }
-    getBuildStatus('test', 'master', getContext(job), function (error, imagePath) {
-      expect(imagePath).to.be('build_passing.png')
+    getBuildStatus('test', 'master', getContext(job), function (error, status) {
+      expect(status.label).to.be('passing')
       done()
     })
   })
 
-  it('should return failed image with failed build', function (done) {
+  it('should return failed label with failed build', function (done) {
     var job = { test_exitcode: 1 }
-    getBuildStatus('test', 'master', getContext(job), function (error, imagePath) {
-      expect(imagePath).to.be('build_failing.png')
+    getBuildStatus('test', 'master', getContext(job), function (error, status) {
+      expect(status.label).to.be('failing')
       done()
     })
   })
 
-  it('should return unknown image when build state unknown', function (done) {
-    getBuildStatus('test', 'master', getContext(null), function (error, imagePath) {
-      expect(imagePath).to.be('build_unknown.png')
+  it('should return unknown label when build state unknown', function (done) {
+    getBuildStatus('test', 'master', getContext(null), function (error, status) {
+      expect(status.label).to.be('unknown')
       done()
     })
   })
 
-  it('should return success image for the default branch ', function (done) {
+  it('should return success label for the default branch ', function (done) {
     var job = { test_exitcode: 0 }
-    getBuildStatus('test', undefined, getContext(job), function (error, imagePath) {
-      expect(imagePath).to.be('build_passing.png')
+    getBuildStatus('test', undefined, getContext(job), function (error, status) {
+      expect(status.label).to.be('passing')
       done()
     })
   })
 
-  it('should return unknown image when job for the specified branch does not exist', function (done) {
+  it('should return unknown label when job for the specified branch does not exist', function (done) {
     var job = { test_exitcode: 0 }
-    getBuildStatus('test', 'develop', getContext(job), function (error, imagePath) {
-      expect(imagePath).to.be('build_unknown.png')
+    getBuildStatus('test', 'develop', getContext(job), function (error, status) {
+      expect(status.label).to.be('unknown')
       done()
+    })
+  })
+
+  describe('response', function () {
+    it('should return success image with successful build', function (done) {
+      var job = { test_exitcode: 0 }
+      var context = getContext(job)
+
+      // fake the express app
+      context.app = {
+        // add a get handler
+        get: function(path, handler) {
+          // fake the request
+          var req = {
+            params: { org: 'test', repo: 'test' },
+            query: { branch: 'master' }
+          }
+
+          // provide a fake sendFile function
+          var res = {
+            sendfile: function(filePath) {
+              expect(filePath).to.contain('build_passing.png')
+              done()
+            }
+          }
+
+          handler(req, res)
+        }
+      }
+
+      var app = require(__dirname + '/../webapp')(context, function() { })
     })
   })
 })
