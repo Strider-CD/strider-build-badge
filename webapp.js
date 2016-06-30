045e18fa213b2390d47b8ac3e30e4f25bcb326c1
James Lambie <jameslambie@gmail.com>
2016-06-30 16:53:22 +0800

    feat: add badge style override to query options

--- webapp.js
+++ webapp.js
@@ -7,20 +7,21 @@ module.exports = function (context, done) {
   context.app.get('/:org/:repo/badge', function (req, res) {
     var name = req.params.org + '/' + req.params.repo
     var branch = req.query.branch
-    var style = req.query.style || 'default'
+    var service = req.query.service || 'default'
+    var style = req.query.style || 'flat-square'
 
     getBuildStatus(name, branch, context, function (error, status) {
       if (error) {
         console.error('[badge] error occured when getting badge: ' + error.message)
       }
 
-      if (style === 'default') {
+      if (service === 'default') {
         res.sendfile(path.join(__dirname, 'images', 'build_' + status.label + '.png'));
       } else {
 
         var badge = {
           subject: 'build',
-          style: 'flat-square'
+          style: style
         }
 
         var badgeUrl = badge.subject + '-' + status.label + '-' + status.color + '.svg?style=' + badge.style;
@@ -31,9 +32,17 @@ module.exports = function (context, done) {
           path: '/badge/' + badgeUrl
         }, function(externalRes) {
           res.setHeader('Cache-Control', 'no-cache');
-          res.setHeader('content-type', 'image/svg+xml;charset=utf-8');
+          res.setHeader('Content-Type', 'image/svg+xml;charset=utf-8');
           externalRes.pipe(res);
         })
+
+        externalReq.on('error', function (err) {
+          console.error('[badge] error occured when requesting badge from https://img.shields.io/' + badgeUrl + '. ' + err);
+
+          // fallback to the image
+          res.sendfile(path.join(__dirname, 'images', 'build_' + status.label + '.png'));
+        });
+
         externalReq.end();
       }
     })
