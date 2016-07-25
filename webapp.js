cbf01e6e310dbca3801d114c2ab09b43a27261d3
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-07-25 09:52:07 +0200

    Modernize

--- webapp.js
+++ webapp.js
@@ -1,22 +1,22 @@
 'use strict';
 
-var path = require('path');
 var getBuildStatus = require('./lib/build-status');
+var path = require('path');
 
 module.exports = function (context, done) {
   context.app.get('/:org/:repo/badge', function (req, res) {
-    var name = req.params.org + '/' + req.params.repo;
+    var name = `${req.params.org}/${req.params.repo}`;
     var branch = req.query.branch;
 
     getBuildStatus(name, branch, context, function (error, imageName) {
       if (error) {
-        console.error('[badge] error occured when getting badge: ' + error.message);
+        console.error(`[badge] error occured when getting badge: ${error.message}`);
       }
-      
+
       res.setHeader('Cache-Control', 'no-cache');
       res.sendfile(path.join(__dirname, 'images', imageName));
     });
   });
-  
+
   done();
 };
